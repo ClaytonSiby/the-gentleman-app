2aab2e229d737b5e84003cb5d4d1ed80b3dfd992
ClaytonSiby <claytonsiby@gmail.com>
2021-04-19 22:54:10 +0200

    Configure scss & introduce the assets directory

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from '../assets/scss/app.module.scss';
+
+const App = () => {
+    return (
+        <div className={`${styles.app}`}>
+            <h2>Hello World</h2>
+            <p>I love programming</p>
+        </div>
+    )
+}
+
+export default App;
