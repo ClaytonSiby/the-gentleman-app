5dbddf1e43eb0ae526b1e1d7b55a8b35b64b67d8
ClaytonSiby <claytonsiby@gmail.com>
2021-05-07 17:59:00 +0200

    Auto fix js errors with eslint code analyzer

--- src/redux/suit/suitReducer.js
+++ src/redux/suit/suitReducer.js
@@ -1,38 +1,38 @@
 import suits from './suitTypes';
 
 const initialState = {
-    loading: false,
-    suits: [],
-    error: ''
-}
+  loading: false,
+  suits: [],
+  error: '',
+};
 
 const suitReducer = (state = initialState, action) => {
-    switch(action.type) {
-        case suits.FETCH_SUITS_REQUEST:
-            return {
-                ...state,
-                loading: true,
-            }
+  switch (action.type) {
+    case suits.FETCH_SUITS_REQUEST:
+      return {
+        ...state,
+        loading: true,
+      };
 
-        case suits.FETCH_SUITS_SUCCESS:
-            return {
-                ...state,
-                loading: false,
-                suits: action.payload,
-                error: ''
-            }
+    case suits.FETCH_SUITS_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        suits: action.payload,
+        error: '',
+      };
 
-        case suits.FETCH_SUITS_FAILURE:
-            return {
-                ...state,
-                loading: false,
-                suits: [],
-                error: action.payload
-            }
+    case suits.FETCH_SUITS_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        suits: [],
+        error: action.payload,
+      };
 
-        default:
-            return state
-    }
-}
+    default:
+      return state;
+  }
+};
 
 export default suitReducer;
