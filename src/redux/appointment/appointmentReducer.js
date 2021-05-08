2def63c96ec3911e984252b25dcef3a9f5d91c4c
ClaytonSiby <claytonsiby@gmail.com>
2021-05-08 12:18:14 +0200

    Fix the js code errors with eslint auto correct

--- src/redux/appointment/appointmentReducer.js
+++ src/redux/appointment/appointmentReducer.js
@@ -1,38 +1,38 @@
 import appointments from './appointmentTypes';
 
 const initialState = {
-    loading: false,
-    appointments: [],
-    error: ''
-}
+  loading: false,
+  appointments: [],
+  error: '',
+};
 
 const appointmentReducer = (state = initialState, action) => {
-    switch(action.type) {
-      case appointments.FETCH_APPOINTMENTS_REQUEST:
-          return {
-              ...state,
-              loading: true
-          }
+  switch (action.type) {
+    case appointments.FETCH_APPOINTMENTS_REQUEST:
+      return {
+        ...state,
+        loading: true,
+      };
 
-      case appointments.FETCH_APPOINTMENTS_SUCCESS:
-          return {
-              ...state,
-              loading: false,
-              appointments: action.payload,
-              error: ''
-          }
+    case appointments.FETCH_APPOINTMENTS_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        appointments: action.payload,
+        error: '',
+      };
 
-      case appointments.FETCHl_APPOINTMENTS_FAILURE:
-          return {
-              ...state,
-              loading: false,
-              appointments: [],
-              error: action.payload
-          }
+    case appointments.FETCHl_APPOINTMENTS_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        appointments: [],
+        error: action.payload,
+      };
 
-      default:
-          return state
-    }
-}
+    default:
+      return state;
+  }
+};
 
 export default appointmentReducer;
