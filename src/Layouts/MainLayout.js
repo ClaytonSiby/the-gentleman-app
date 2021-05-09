a1413de647d1db281214db69bc5f4ffeb824c927
ClaytonSiby <claytonsiby@gmail.com>
2021-05-09 22:01:27 +0200

    Add the MainLayout component

---
+++ src/Layouts/MainLayout.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+const MainLayout = ({ children }) => {
+    return (
+        <div>
+            <Header />
+            {
+                children
+            }
+            <Footer />
+        </div>
+    )
+}
+
+export default MainLayout;
