b21350a99c0aa2b67a4a81800204e2e394ee7237
ClaytonSiby <claytonsiby@gmail.com>
2021-05-10 12:41:42 +0200

    Add signup form

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { Form } from 'react-bootstrap';
 
 const SignUp = () => (
-  <div>Signing Up!</div>
+  <Form>
+    <Form.Group>
+      <Form.Control type="text" placeholder="Enter name here.." />
+    </Form.Group>
+    <Form.Group>
+      <Form.Control type="email" placeholder="Enter email" />
+    </Form.Group>
+    <Form.Group>
+      <Form.Control  type="password" placeholder="password****" />
+      <Form.Control  type="password" placeholder="confirm password****" />
+    </Form.Group>
+    <Form.Group>
+      <button type="submit" className="btn btn-block btn-md">SignUp</button>
+    </Form.Group>
+  </Form>
 );
 
 export default SignUp;
