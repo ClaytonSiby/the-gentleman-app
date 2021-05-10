306f11ee74b37b28027607a18fd3e4e95d57d77c
ClaytonSiby <claytonsiby@gmail.com>
2021-05-10 13:28:28 +0200

    Add the login form

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,28 @@
 import React from 'react'
+import { Container, Form } from 'react-bootstrap'
 
 const Login = () => {
   return (
-    <div>
+    <Container>
       <h1>Login</h1>
-    </div>
+      <Form>
+        <Form.Group controlId='formBasicEmail'>
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type='email' placeholder='Enter email' />
+          <Form.Text className='text-muted'>
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group controlId='formBasicPassword'>
+          <Form.Label>Password</Form.Label>
+          <Form.Control type='password' placeholder='Password' />
+        </Form.Group>
+        <button className='btn btn-primary' type='submit'>
+          Submit
+        </button>
+      </Form>
+    </Container>
   )
 }
 
