cc62fa99c9a62ac7670a1ee905fe3fbb7dbc471c
ClaytonSiby <claytonsiby@gmail.com>
2021-05-10 21:55:41 +0200

    Refactor redux-persist dependency import

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,14 @@
-import thunk from 'redux-thunk';
-import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import { persistStore } from 'redux-persit';
-import persistReducer from './rootReducer';
+import thunk from 'redux-thunk'
+import { createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import { persistStore } from 'redux-persist'
+import persistReducer from './rootReducer'
 
-const store = createStore(persistReducer, composeWithDevTools(applyMiddleware(thunk)));
+const store = createStore(
+  persistReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+)
 
-const persistor = persistStore(store);
+const persistor = persistStore(store)
 
-export default { store, persistor };
+export { store, persistor }
