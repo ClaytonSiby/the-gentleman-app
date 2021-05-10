fd2056dd69f1750c6c5d52669494d5058e68360f
ClaytonSiby <claytonsiby@gmail.com>
2021-05-10 20:57:56 +0200

    Update the SignUp component

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,28 +1,41 @@
-import React from 'react';
-import { Form } from 'react-bootstrap';
-import styles from '../assets/scss/signup.module.scss';
+import React, { useRef } from 'react'
+import { Form, Button } from 'react-bootstrap'
+import styles from '../assets/scss/signup.module.scss'
 
-const SignUp = () => (
-  <div className={`${styles.signup}`}>
-    <h1>Sign Up</h1>
-    <Form>
-      <Form.Group controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We&apos;ll never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
+const SignUp = () => {
+  const username = useRef(null)
+  const email = useRef(null)
 
-      <Form.Group controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-      <button className="btn btn-primary" type="submit">
-        Submit
-      </button>
-    </Form>
-  </div>
-);
+  const buttonClick = () => {
+    username.current.focus();
+  }
 
-export default SignUp;
+  return (
+    <div className={`${styles.signup}`}>
+      <h1>Sign Up</h1>
+      <Form>
+        <Form.Group controlId='username'>
+          <Form.Label>Name</Form.Label>
+          <Form.Control ref={username} type='text' placeholder='Enter email' />
+        </Form.Group>
+        <Form.Group controlId='email'>
+          <Form.Label>Email address</Form.Label>
+          <Form.Control ref={email} type='email' placeholder='Enter email' />
+          <Form.Text className='text-muted'>
+            We&apos;ll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group controlId='password'>
+          <Form.Label>Password</Form.Label>
+          <Form.Control type='password' placeholder='Password' />
+        </Form.Group>
+        <Button onClick={buttonClick} variant='primary' className="my-3" size='md' block>
+          Sign Up
+        </Button>
+      </Form>
+    </div>
+  )
+}
+
+export default SignUp
