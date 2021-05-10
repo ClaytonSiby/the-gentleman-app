ea9d65958b94d93a95253defd1c3ae7f18c12a60
ClaytonSiby <claytonsiby@gmail.com>
2021-05-10 20:27:05 +0200

    Auto fix errors with eslint

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,21 +2,21 @@ import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
 
 const Header = () => (
-    <Navbar sticky="top" collapseOnSelect expand='lg' bg='dark' variant='dark'>
-      <Navbar.Brand href='#home'>React-Bootstrap</Navbar.Brand>
-      <Navbar.Toggle aria-controls='responsive-navbar-nav' />
-      <Navbar.Collapse id='responsive-navbar-nav'>
-        <Nav className='mr-auto'>
-          <Nav.Link href='/appointments'>Appointments</Nav.Link>
-        </Nav>
-        <Nav className="mr-auto">
-          <Nav.Link href='/login'>Login</Nav.Link>
-          <Nav.Link eventKey={2} href='/signup'>
-            SignUp
-          </Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
-)
+  <Navbar sticky="top" collapseOnSelect expand="lg" bg="dark" variant="dark">
+    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+    <Navbar.Collapse id="responsive-navbar-nav">
+      <Nav className="mr-auto">
+        <Nav.Link href="/appointments">Appointments</Nav.Link>
+      </Nav>
+      <Nav className="mr-auto">
+        <Nav.Link href="/login">Login</Nav.Link>
+        <Nav.Link eventKey={2} href="/signup">
+          SignUp
+        </Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
+);
 
-export default Header
+export default Header;
