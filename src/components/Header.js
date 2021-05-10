67a9b57147cc7545689fa9d633b9053d3a09d594
ClaytonSiby <claytonsiby@gmail.com>
2021-05-10 13:02:58 +0200

    Update the Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
 
 const Header = () => (
-  <div>
-    <h1>This is the Header!</h1>
-  </div>
-);
+  <Navbar collapseOnSelect expand='lg' bg='dark' variant='dark'>
+    <Navbar.Brand href='#home'>React-Bootstrap</Navbar.Brand>
+    <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+    <Navbar.Collapse id='responsive-navbar-nav'>
+      <Nav className='mr-auto'>
+        <Nav.Link href='#features'>Features</Nav.Link>
+        <Nav.Link href='#pricing'>Pricing</Nav.Link>
+      </Nav>
+      <Nav>
+        <Nav.Link href='#deets'>More deets</Nav.Link>
+        <Nav.Link eventKey={2} href='#memes'>
+          Dank memes
+        </Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
+)
 
-export default Header;
+export default Header
