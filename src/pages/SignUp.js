dc6001fab2c53a792612b035e52b5c27626f4ee7
ClaytonSiby <claytonsiby@gmail.com>
2021-05-10 12:48:39 +0200

    Complete signup form

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,22 +1,26 @@
-import React from 'react';
-import { Form } from 'react-bootstrap';
+import React from 'react'
+import { Form, Container } from 'react-bootstrap'
 
 const SignUp = () => (
-  <Form>
-    <Form.Group>
-      <Form.Control type="text" placeholder="Enter name here.." />
-    </Form.Group>
-    <Form.Group>
-      <Form.Control type="email" placeholder="Enter email" />
-    </Form.Group>
-    <Form.Group>
-      <Form.Control type="password" placeholder="password****" />
-      <Form.Control type="password" placeholder="confirm password****" />
-    </Form.Group>
-    <Form.Group>
-      <button type="submit" className="btn btn-block btn-md">SignUp</button>
-    </Form.Group>
-  </Form>
-);
+  <Container>
+    <Form>
+      <Form.Group className='my-1'>
+        <Form.Control type='text' placeholder='Enter name here..' />
+      </Form.Group>
+      <Form.Group className='my-1'>
+        <Form.Control type='email' placeholder='Enter email' />
+      </Form.Group>
+      <Form.Group className='my-1'>
+        <Form.Control type='password' placeholder='password****'/>
+        <Form.Control type='password' placeholder='confirm password****' className="my-1"/>
+      </Form.Group>
+      <Form.Group className='my-1'>
+        <button type='submit' className='btn btn-block btn-md'>
+          SignUp
+        </button>
+      </Form.Group>
+    </Form>
+  </Container>
+)
 
-export default SignUp;
+export default SignUp
