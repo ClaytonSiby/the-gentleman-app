b311c049466cb1c80fdf49344a413f9e9557ed75
ClaytonSiby <claytonsiby@gmail.com>
2021-05-10 13:04:36 +0200

    Auto correct errors with eslint

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,22 +2,22 @@ import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
 
 const Header = () => (
-  <Navbar collapseOnSelect expand='lg' bg='dark' variant='dark'>
-    <Navbar.Brand href='#home'>React-Bootstrap</Navbar.Brand>
-    <Navbar.Toggle aria-controls='responsive-navbar-nav' />
-    <Navbar.Collapse id='responsive-navbar-nav'>
-      <Nav className='mr-auto'>
-        <Nav.Link href='#features'>Features</Nav.Link>
-        <Nav.Link href='#pricing'>Pricing</Nav.Link>
+  <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+    <Navbar.Collapse id="responsive-navbar-nav">
+      <Nav className="mr-auto">
+        <Nav.Link href="#features">Features</Nav.Link>
+        <Nav.Link href="#pricing">Pricing</Nav.Link>
       </Nav>
       <Nav>
-        <Nav.Link href='#deets'>More deets</Nav.Link>
-        <Nav.Link eventKey={2} href='#memes'>
+        <Nav.Link href="#deets">More deets</Nav.Link>
+        <Nav.Link eventKey={2} href="#memes">
           Dank memes
         </Nav.Link>
       </Nav>
     </Navbar.Collapse>
   </Navbar>
-)
+);
 
-export default Header
+export default Header;
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,26 +1,26 @@
-import React from 'react'
-import { Form, Container } from 'react-bootstrap'
+import React from 'react';
+import { Form, Container } from 'react-bootstrap';
 
 const SignUp = () => (
   <Container>
     <Form>
-      <Form.Group className='my-1'>
-        <Form.Control type='text' placeholder='Enter name here..' />
+      <Form.Group className="my-1">
+        <Form.Control type="text" placeholder="Enter name here.." />
       </Form.Group>
-      <Form.Group className='my-1'>
-        <Form.Control type='email' placeholder='Enter email' />
+      <Form.Group className="my-1">
+        <Form.Control type="email" placeholder="Enter email" />
       </Form.Group>
-      <Form.Group className='my-1'>
-        <Form.Control type='password' placeholder='password****'/>
-        <Form.Control type='password' placeholder='confirm password****' className="my-1"/>
+      <Form.Group className="my-1">
+        <Form.Control type="password" placeholder="password****" />
+        <Form.Control type="password" placeholder="confirm password****" className="my-1" />
       </Form.Group>
-      <Form.Group className='my-1'>
-        <button type='submit' className='btn btn-block btn-md'>
+      <Form.Group className="my-1">
+        <button type="submit" className="btn btn-block btn-md">
           SignUp
         </button>
       </Form.Group>
     </Form>
   </Container>
-)
+);
 
-export default SignUp
+export default SignUp;
