09717d7e27e65263d5a59d976503ae8cccc3edeb
ClaytonSiby <claytonsiby@gmail.com>
2021-05-12 22:28:52 +0200

    Add the history feature for the routing system

--- src/App.js
+++ src/App.js
@@ -1,45 +1,60 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import { Container } from 'react-bootstrap';
-import Homepage from './pages/Homepage';
-import SignUp from './pages/SignUp';
-import Login from './pages/Login';
-import HomepageLayout from './Layouts/HomepageLayout';
-import MainLayout from './Layouts/MainLayout';
-import styles from './assets/scss/app.module.scss';
+import React from 'react'
+import { Route, Router, Switch } from 'react-router-dom'
+import { createBrowserHistory } from 'history'
+import { Container } from 'react-bootstrap'
+import Homepage from './pages/Homepage'
+import SignUp from './pages/SignUp'
+import Login from './pages/Login'
+import Suits from './components/Suits'
+import HomepageLayout from './Layouts/HomepageLayout'
+import MainLayout from './Layouts/MainLayout'
+import styles from './assets/scss/app.module.scss'
+
+const history = createBrowserHistory()
 
 const App = () => (
   <Container className={`${styles.app}`}>
-    <Switch>
-      <Route
-        exact
-        path="/"
-        render={() => (
-          <HomepageLayout>
-            <Homepage />
-          </HomepageLayout>
-        )}
-      />
+    <Router history={history}>
+      <Switch>
+        <Route
+          exact
+          path='/'
+          render={() => (
+            <HomepageLayout>
+              <Homepage />
+            </HomepageLayout>
+          )}
+        />
 
-      <Route
-        path="/signup"
-        render={() => (
-          <MainLayout>
-            <SignUp />
-          </MainLayout>
-        )}
-      />
+        <Route
+          path='/signup'
+          render={() => (
+            <MainLayout>
+              <SignUp />
+            </MainLayout>
+          )}
+        />
 
-      <Route
-        path="/login"
-        render={() => (
-          <MainLayout>
-            <Login />
-          </MainLayout>
-        )}
-      />
-    </Switch>
+        <Route
+          path='/login'
+          render={() => (
+            <MainLayout>
+              <Login />
+            </MainLayout>
+          )}
+        />
+
+        <Route
+          path='/suits'
+          render={() => (
+            <MainLayout>
+              <Suits />
+            </MainLayout>
+          )}
+        />
+      </Switch>
+    </Router>
   </Container>
-);
+)
 
-export default App;
+export default App
