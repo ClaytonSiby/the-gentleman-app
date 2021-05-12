7a675d0eb369cc8d0c5f80507beae80a6e9a1f59
ClaytonSiby <claytonsiby@gmail.com>
2021-05-12 22:29:44 +0200

    Configure the redux-logger dependency

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,10 +3,11 @@ import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { persistStore } from 'redux-persist';
 import persistReducer from './rootReducer';
+import logger from 'redux-logger';
 
 const store = createStore(
   persistReducer,
-  composeWithDevTools(applyMiddleware(thunk)),
+  composeWithDevTools(applyMiddleware(thunk, logger)),
 );
 
 const persistor = persistStore(store);
