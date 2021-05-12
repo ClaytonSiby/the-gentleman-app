62569db4e216a3a4f1beb25f0a366ddabff2bfff
ClaytonSiby <claytonsiby@gmail.com>
2021-05-12 09:35:49 +0200

    Auto fix errors with eslint

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,14 @@
-import thunk from 'redux-thunk'
+import thunk from 'redux-thunk';
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import { persistStore } from 'redux-persist'
-import persistReducer from './rootReducer'
+import { persistStore } from 'redux-persist';
+import persistReducer from './rootReducer';
 
 const store = createStore(
   persistReducer,
-  composeWithDevTools(applyMiddleware(thunk))
-)
+  composeWithDevTools(applyMiddleware(thunk)),
+);
 
-const persistor = persistStore(store)
+const persistor = persistStore(store);
 
-export { store, persistor }
+export { store, persistor };
