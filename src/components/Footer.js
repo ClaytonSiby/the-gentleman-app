314570db22be901900c555367074c020c5c45706
ClaytonSiby <claytonsiby@gmail.com>
2021-05-13 17:02:41 +0200

    Add background image for the login and signup pages

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from '../assets/scss/footer.module.scss';
 
 const Footer = () => (
-  <div className={`p-2 mt-4 text-center ${styles.footerContainer}`}>
+  <div className={`pt-4 text-center ${styles.footerContainer}`}>
     <p>Developed by Clayton Siby. &copy; 2021. All rights reserved.</p>
   </div>
 );
