0ca257d584ce3906f678893d471bb311153b5992
ClaytonSiby <claytonsiby@gmail.com>
2021-05-13 09:43:22 +0200

    Restructure the Suits component & add css styles for a responsive layout

--- src/components/Suits.js
+++ src/components/Suits.js
@@ -4,6 +4,7 @@ import { connect, useSelector } from 'react-redux'
 import ReactLoading from 'react-loading'
 import { Redirect } from 'react-router-dom'
 import { Container, Row, Col } from 'react-bootstrap'
+import styles from '../assets/scss/suits.module.scss'
 
 const Suits = ({ suitsData, suitsRequest }) => {
   const [loadingComplete, setLoadingComplete] = useState(false)
@@ -17,20 +18,33 @@ const Suits = ({ suitsData, suitsRequest }) => {
   }, [])
 
   return (
-    <Container>
+    <Container className="text-light">
       {!loadingComplete ? (
         <div>
           <ReactLoading type='bars' color='green' height={70} width={80} />
         </div>
       ) : (
         <Row>
-          {
-            suitsData && suitsData.suits && suitsData.suits.map(data => (
-              <Col xs={12} sm={12} md={3} key={Math.floor(Math.random() * 1000)}>
-                { data.name }
-              </Col>
-            ) )
-          }
+          <Col sm={12} className={`${styles.header}`}></Col>
+          <Col sm={12}>
+          <div className={`${styles.suitsContainer} p-0`}>
+          {suitsData &&
+            suitsData.suits &&
+            suitsData.suits.map(data => (
+              <div
+                className={`${styles.suitThumbnail} my-1 p-2`}
+                key={data.id}
+                style={{ 
+                  backgroundImage: `linear-gradient(rgba(0, 0, 0, .4), rgba(0, 0, 0, .6)),url(${data.imageUrl})`,
+                  borderRadius: '15px',
+                  marginLeft: '.5rem'
+                }}
+              >
+                
+              </div>
+            ))}
+        </div>
+          </Col>
         </Row>
       )}
     </Container>
