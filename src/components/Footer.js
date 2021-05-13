efd7a05d23544f4caa26235ff7b14e8ee7a4e6a5
ClaytonSiby <claytonsiby@gmail.com>
2021-05-13 16:45:23 +0200

    Auto fix errors with eslint & stylelint

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import styles from '../assets/scss/footer.module.scss';
 
 const Footer = () => (
   <div className={`p-2 mt-4 text-center ${styles.footerContainer}`}>
-      <p>Developed by Clayton Siby. &copy; 2021. All rights reserved.</p>
+    <p>Developed by Clayton Siby. &copy; 2021. All rights reserved.</p>
   </div>
 );
 
