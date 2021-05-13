4e095e9ec913b841c370a01b0eabb8fae1722668
ClaytonSiby <claytonsiby@gmail.com>
2021-05-13 19:33:27 +0200

    Add valid hyper links to social media profiles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,17 +6,17 @@ const Footer = () => (
     <p>Developed by Clayton Siby. &copy;copyright 2021. All rights reserved.</p>
     <p>
       <span className={`${styles.socialIcon}`}>
-        <a href="">
+        <a href="https://github.com/ClaytonSiby" target="_blank" rel="noreferrer">
           <i className="fab fa-github" />
         </a>
       </span>
       <span className={`${styles.socialIcon}`}>
-        <a href="">
+        <a href="https://www.linkedin/in/clayton-siby" target="_blank" rel="noreferrer">
           <i className="fab fa-linkedin-in" />
         </a>
       </span>
       <span className={`${styles.socialIcon}`}>
-        <a href="">
+        <a href="https://twitter.com/ClaytonSiby" target="_blank" rel="noreferrer">
           <i className="fab fa-twitter" />
         </a>
       </span>
