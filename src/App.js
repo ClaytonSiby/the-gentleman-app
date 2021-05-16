165bc3a2b5f852ab4f9f6617ffbce339d8633d78
ClaytonSiby <claytonsiby@gmail.com>
2021-05-16 19:59:11 +0200

    Fix the routing bug

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import { Route, Router, Switch } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
 import { Container } from 'react-bootstrap';
-import Homepage from './pages/Homepage';
-import Appointments from './pages/Appointments';
-import SignUp from './pages/SignUp';
-import Login from './pages/Login';
-import Suits from './components/Suits';
-import HomepageLayout from './Layouts/HomepageLayout';
-import MainLayout from './Layouts/MainLayout';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import RouteGenerator from './containers/RouteGenerator';
 import styles from './assets/scss/app.module.scss';
 
 const history = createBrowserHistory();
@@ -16,67 +12,9 @@ const history = createBrowserHistory();
 const App = () => (
   <Container fluid className={`${styles.app}`}>
     <Router history={history}>
-      <Switch>
-        <Route
-          exact
-          path="/"
-          render={() => (
-            <HomepageLayout>
-              <Homepage />
-            </HomepageLayout>
-          )}
-        />
-
-        <Route
-          exact
-          path="/signup"
-          render={() => (
-            <MainLayout>
-              <SignUp />
-            </MainLayout>
-          )}
-        />
-
-        <Route
-          exact
-          path="/login"
-          render={() => (
-            <MainLayout>
-              <Login />
-            </MainLayout>
-          )}
-        />
-
-        <Route
-          exact
-          path="/suits"
-          render={() => (
-            <MainLayout>
-              <Suits />
-            </MainLayout>
-          )}
-        />
-
-        <Route
-          exact
-          path="/appointments"
-          render={() => (
-            <MainLayout>
-              <Appointments />
-            </MainLayout>
-          )}
-        />
-
-        <Route
-          exact
-          path="/logout"
-          render={() => {
-            window.location.reload();
-            localStorage.setItem('userToken', undefined);
-            window.location.href = '/login';
-          }}
-        />
-      </Switch>
+      <Header />
+        <RouteGenerator />
+      <Footer />
     </Router>
   </Container>
 );
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+// import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import App from './App';
@@ -12,9 +12,7 @@ ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate persistor={persistor}>
-        <BrowserRouter>
           <App />
-        </BrowserRouter>
       </PersistGate>
     </Provider>
   </React.StrictMode>,
