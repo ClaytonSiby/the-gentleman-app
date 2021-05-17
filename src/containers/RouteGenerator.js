5905da6813733d7fab5e630d1aea0132b395b83d
ClaytonSiby <claytonsiby@gmail.com>
2021-05-17 20:06:16 +0200

    Add a route to a suite item page

--- src/containers/RouteGenerator.js
+++ src/containers/RouteGenerator.js
@@ -4,15 +4,17 @@ import Homepage from '../pages/Homepage';
 import Suits from '../components/Suits';
 import Login from '../pages/Login';
 import SignUp from '../pages/SignUp';
+import SuitItem from '../pages/SuitItem';
 import Appointments from '../pages/Appointments';
 
 const RouteGenerator = () => (
   <Switch>
-    <Route exact path="/" component={Homepage} />
-    <Route path="/suits" component={Suits} />
-    <Route path="/login" component={Login} />
-    <Route path="/signup" component={SignUp} />
-    <Route path="/appointments" component={Appointments} />
+    <Route exact exact path="/" component={Homepage} />
+    <Route exact path="/suits" component={Suits} />
+    <Route exact path="/login" component={Login} />
+    <Route exact path="/signup" component={SignUp} />
+    <Route exact path="/appointments" component={Appointments} />
+    <Route exact path="/suits/:id" component={SuitItem} />
     <Route
       path="/logout"
       render={() => {
