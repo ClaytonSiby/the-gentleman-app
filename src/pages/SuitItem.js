9ae117e482d30132c70efeefa46c4d3d8e8a7ab9
ClaytonSiby <claytonsiby@gmail.com>
2021-05-17 21:30:54 +0200

    Auto fix errors with eslint

--- src/pages/SuitItem.js
+++ src/pages/SuitItem.js
@@ -1,10 +1,12 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import { Container, Col, Row, Table } from 'react-bootstrap'
-import styles from '../assets/scss/suitItem.module.scss'
+import React from 'react';
+import { useSelector } from 'react-redux';
+import {
+  Container, Col, Row,
+} from 'react-bootstrap';
+import styles from '../assets/scss/suitItem.module.scss';
 
 const SuitItem = () => {
-  const suitItemData = useSelector(state => state.suitItem.suit)
+  const suitItemData = useSelector((state) => state.suitItem.suit);
   return (
     <Container className={`${styles.suitContainer}`}>
       <Row>
@@ -13,22 +15,38 @@ const SuitItem = () => {
           md={6}
           className={`${styles.suitImage}`}
           style={{
-            background: `linear-gradient(rgba(0,0,0,.7), rgba(0,0,0,.7)), url(${suitItemData.imageUrl})`
+            background: `linear-gradient(rgba(0,0,0,.7), rgba(0,0,0,.7)), url(${suitItemData.imageUrl})`,
           }}
-        ></Col>
+        />
         <Col sm={12} md={6} className={`${styles.suitDescription} p-3`}>
-          <h4> {suitItemData.name} </h4>
+          <h4>
+            {' '}
+            {suitItemData.name}
+            {' '}
+          </h4>
           <div>
-            <p>available color: { suitItemData.color }</p>
-            <p>price: ${ suitItemData.price }</p>
-            <p>available size: { suitItemData.size }</p>
-            <p>type: { suitItemData.suit_type }</p>
+            <p>
+              available color:
+              { suitItemData.color }
+            </p>
+            <p>
+              price: $
+              { suitItemData.price }
+            </p>
+            <p>
+              available size:
+              { suitItemData.size }
+            </p>
+            <p>
+              type:
+              { suitItemData.suit_type }
+            </p>
             <p>{ suitItemData.description }</p>
           </div>
         </Col>
       </Row>
     </Container>
-  )
-}
+  );
+};
 
-export default SuitItem
+export default SuitItem;
