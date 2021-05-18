2c908418f089b1bec0fb74c657923c4cfb825114
ClaytonSiby <claytonsiby@gmail.com>
2021-05-18 18:41:01 +0200

    Add the structure for the Appointments page

--- src/pages/Appointments.js
+++ src/pages/Appointments.js
@@ -1,34 +1,55 @@
-import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import ReactLoading from 'react-loading';
-import fetchAppointments from '../redux/appointments/get/appointmentActions';
-import styles from '../assets/scss/appointments.module.scss';
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Row, Col, Card, Button } from 'react-bootstrap'
+import ReactLoading from 'react-loading'
+import fetchAppointments from '../redux/appointments/get/appointmentActions'
+import styles from '../assets/scss/appointments.module.scss'
 
 const Appointments = () => {
-  const [loadingComplete, setLoadingComplete] = useState(false);
-  const dispatch = useDispatch();
-  const authToken = useSelector((state) => state.signup.user.auth_token);
+  const [loadingComplete, setLoadingComplete] = useState(false)
+  const dispatch = useDispatch()
+  const authToken = useSelector(state => state.signup.user.auth_token)
   useEffect(() => {
     setTimeout(() => {
-      (async () => {
-        await dispatch(fetchAppointments(authToken));
-      })();
-      setLoadingComplete(true);
-    }, 3000);
-  }, []);
+      ;(async () => {
+        await dispatch(fetchAppointments(authToken))
+      })()
+      setLoadingComplete(true)
+    }, 3000)
+  }, [])
 
-  const appointments = useSelector((state) => state.appointments);
+  const appointments = useSelector(state => state.appointments)
   return (
-    <div>
+    <Row>
       {!loadingComplete ? (
         <div className={`${styles.loading}`}>
-          <ReactLoading type="bars" color="grey" height={70} width={80} />
+          <ReactLoading type='bars' color='grey' height={70} width={80} />
+        </div>
+      ) : appointments.data.length < 1 ? (
+        <div className='text-center bg-warning'>
+          <p>You do not have any appointments.</p>
         </div>
       ) : (
-        <div>{JSON.stringify(appointments)}</div>
+        appointments.data &&
+        appointments.data.map(data => (
+          <Col xs={12} md={3}>
+            <Card style={{ width: '100%', minHeight: '30vh' }}>
+              <Card.Img variant='top' src={`${data.imageUrl}`} />
+              <Card.Body>
+                <Card.Title>{ data.name }</Card.Title>
+                <Card.Text>
+                  {
+                    data.description
+                  }
+                </Card.Text>
+                <Button variant='primary'>Cancel Appointment</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))
       )}
-    </div>
-  );
-};
+    </Row>
+  )
+}
 
-export default Appointments;
+export default Appointments
