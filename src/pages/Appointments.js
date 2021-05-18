b16988aad2a98b18b40a073d2661e9d2b01bfd7e
ClaytonSiby <claytonsiby@gmail.com>
2021-05-18 12:40:21 +0200

    Update the Appointments page to display a loading animation on the center of the page

--- src/pages/Appointments.js
+++ src/pages/Appointments.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import fetchAppointments from '../redux/appointments/get/appointmentActions';
+import ReactLoading from 'react-loading';
+import styles from '../assets/scss/appointments.module.scss';
 
 const Appointments = () => {
   const [loadingComplete, setLoadingComplete] = useState(false);
@@ -12,15 +14,15 @@ const Appointments = () => {
         await dispatch(fetchAppointments(authToken));
       })();
       setLoadingComplete(true);
-    }, 2000);
+    }, 3000);
   }, []);
 
   const appointments = useSelector((state) => state.appointments.appointments);
   return (
     <div>
       {!loadingComplete ? (
-        <div>
-          <h2>Loading....</h2>
+        <div className={`${styles.loading}`}>
+          <ReactLoading type="bars" color="grey" height={70} width={80} />
         </div>
       ) : (
         <div>{JSON.stringify(appointments)}</div>
