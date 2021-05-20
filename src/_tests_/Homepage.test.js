aabb430acf81e27b1fdf3b60ec84d8669153d630
ClaytonSiby <claytonsiby@gmail.com>
2021-05-20 09:52:59 +0200

    Add test suite for the Homepage

---
+++ src/_tests_/Homepage.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Homepage from '../pages/Homepage';
+
+describe('Homepage', () => {
+    test('Renders the Homepage component successfully', () => {
+        render(<Homepage />)
+    })
+})
