835874654f4a67e59303aa50285f92a381c431df
ClaytonSiby <claytonsiby@gmail.com>
2021-05-20 10:01:35 +0200

    Auto correct code with eslint analyzer

--- src/_tests_/Login.test.js
+++ src/_tests_/Login.test.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import { Provider } from 'react-redux'
-import { render } from '@testing-library/react'
-import Login from '../pages/Login'
-import store from './Suits.test'
+import React from 'react';
+import { Provider } from 'react-redux';
+import { render } from '@testing-library/react';
+import Login from '../pages/Login';
+import store from './Suits.test';
 
 describe('Login', () => {
   it('Renders the Login page successfully', () => {
     render(
       <Provider store={store}>
         <Login />
-      </Provider>
-    )
-  })
-})
+      </Provider>,
+    );
+  });
+});
