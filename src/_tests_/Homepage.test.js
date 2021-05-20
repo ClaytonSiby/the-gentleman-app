40193fc2190c1e5e3183e02c510aafdd7d6df427
ClaytonSiby <claytonsiby@gmail.com>
2021-05-20 10:40:17 +0200

    Implement snapshot test for the Homepage component

--- src/_tests_/Homepage.test.js
+++ src/_tests_/Homepage.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import renderer from 'react-test-renderer';
 import Homepage from '../pages/Homepage';
 
 describe('Homepage', () => {
   test('Renders the Homepage component successfully', () => {
     render(<Homepage />);
   });
+
+  test('renders consistently to the DOM', () => {
+    const test = renderer.create(<Homepage />).toJSON();
+
+    expect(test).toMatchSnapshot();
+  })
 });
