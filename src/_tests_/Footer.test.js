1d3706184a75de937fa6338c5dcab120daef399a
ClaytonSiby <claytonsiby@gmail.com>
2021-05-20 09:27:16 +0200

    Add test suite for the Footer component

---
+++ src/_tests_/Footer.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Footer from '../components/Footer';
+
+describe('Footer', () => {
+    test('Footer renders successfully', () => {
+        render(<Footer />)
+    })
+})
