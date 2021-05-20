d0a4df3396ad4a74cf28ee4d51a3e3f3e2b011dd
ClaytonSiby <claytonsiby@gmail.com>
2021-05-20 09:53:31 +0200

    Fix errors with eslint

--- src/_tests_/Homepage.test.js
+++ src/_tests_/Homepage.test.js
@@ -3,7 +3,7 @@ import { render } from '@testing-library/react';
 import Homepage from '../pages/Homepage';
 
 describe('Homepage', () => {
-    test('Renders the Homepage component successfully', () => {
-        render(<Homepage />)
-    })
-})
+  test('Renders the Homepage component successfully', () => {
+    render(<Homepage />);
+  });
+});
