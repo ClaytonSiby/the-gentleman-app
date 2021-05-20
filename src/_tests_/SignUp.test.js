850a312995c29b8897c75cc7e509beb5a7a98ca8
ClaytonSiby <claytonsiby@gmail.com>
2021-05-20 10:04:56 +0200

    Add test suite for the SignUp page

---
+++ src/_tests_/SignUp.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { render } from '@testing-library/react';
+import SignUp from '../pages/SignUp';
+import store from './Suits.test';
+
+describe('SignUp', () => {
+    it('Renders the SignUp page successfully', () => {
+        render(
+            <Provider store={store}>
+                <SignUp />
+            </Provider>
+        )
+    })
+})
