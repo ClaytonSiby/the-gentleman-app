10a2ca9493042b734819ff1141d4525f7ac08f8f
ClaytonSiby <claytonsiby@gmail.com>
2021-05-20 10:09:16 +0200

    Add test suite for the SuitItem page

---
+++ src/_tests_/SuitItem.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { render } from '@testing-library/react';
+import SuitItem from '../pages/SuitItem';
+import store from './Suits.test';
+
+describe('SuitItem', () => {
+    it('Renders the SuitItem page successfully', () => {
+        render(
+            <Provider store={store}>
+                <SuitItem />
+            </Provider>
+        )
+    })
+})
