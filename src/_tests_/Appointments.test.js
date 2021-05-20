58bee0c5e1bb8fb663123a451aa2be7b2b5b4aa4
ClaytonSiby <claytonsiby@gmail.com>
2021-05-20 09:49:14 +0200

    Add test suite for the Appointments component

---
+++ src/_tests_/Appointments.test.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Provider } from 'react-redux'
+import { render } from '@testing-library/react'
+import Appointments from '../pages/Appointments'
+import store from './Suits.test'
+
+describe('Appointments', () => {
+  test('Renders the Appointments component successfully', () => {
+    render(
+      <Provider store={store}>
+        <Appointments />
+      </Provider>
+    )
+  })
+})
--- src/_tests_/Suits.test.js
+++ src/_tests_/Suits.test.js
@@ -38,3 +38,5 @@ describe('Suits', () => {
     );
   });
 });
+
+export default store;
