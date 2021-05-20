9fe39ae64533cd4a8c8a32c48a7f1fa66fcaa4b4
ClaytonSiby <claytonsiby@gmail.com>
2021-05-20 10:18:13 +0200

    Auto correct errors with eslint

--- src/_tests_/App.test.js
+++ src/_tests_/App.test.js
@@ -3,7 +3,7 @@ import { render } from '@testing-library/react';
 import App from '../App';
 
 describe('RouteGenerator', () => {
-    it('Renders the RouteGenerator successfully', () => {
-        render(<App />)
-    })
-})
+  it('Renders the RouteGenerator successfully', () => {
+    render(<App />);
+  });
+});
