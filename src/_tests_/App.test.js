da116751f8a7f2e27f4f685ad0542188fb0a9ecc
ClaytonSiby <claytonsiby@gmail.com>
2021-05-20 10:27:37 +0200

    Add snapshot test for the App component

--- src/_tests_/App.test.js
+++ src/_tests_/App.test.js
@@ -1,9 +1,16 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from '../App';
+import React from 'react'
+import { render } from '@testing-library/react'
+import renderer from 'react-test-renderer'
+import App from '../App'
 
 describe('RouteGenerator', () => {
   it('Renders the RouteGenerator successfully', () => {
-    render(<App />);
-  });
-});
+    render(<App />)
+  })
+
+  it('renders the output consistently to the DOM', () => {
+    const test = renderer.create(<App />).toJSON()
+
+    expect(test).toMatchSnapshot()
+  })
+})
