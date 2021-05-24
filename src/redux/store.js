ca9a53988ee6a9527b0d74f42df56a89acef6b7f
ClaytonSiby <claytonsiby@gmail.com>
2021-05-24 23:51:31 +0200

    Remove redux-logger from the redux store for production release

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,12 +2,11 @@ import thunk from 'redux-thunk';
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { persistStore } from 'redux-persist';
-import logger from 'redux-logger';
 import persistReducer from './rootReducer';
 
 const store = createStore(
   persistReducer,
-  composeWithDevTools(applyMiddleware(thunk, logger)),
+  composeWithDevTools(applyMiddleware(thunk)),
 );
 
 const persistor = persistStore(store);
