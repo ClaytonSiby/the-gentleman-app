df98beac55ca39b31012ca1fe2c38ef470bc20b3
ClaytonSiby <claytonsiby@gmail.com>
2021-05-31 10:26:35 +0200

    Remove debugging logger from the redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,12 @@
 import thunk from 'redux-thunk';
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import { logger } from 'redux-logger';
 import { persistStore } from 'redux-persist';
 import persistReducer from './rootReducer';
 
 const store = createStore(
   persistReducer,
-  composeWithDevTools(applyMiddleware(logger, thunk)),
+  composeWithDevTools(applyMiddleware(thunk)),
 );
 
 const persistor = persistStore(store);
