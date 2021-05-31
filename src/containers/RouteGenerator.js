4676e8145ad196b43e8d291e2f7796d31a09d697
ClaytonSiby <claytonsiby@gmail.com>
2021-05-31 09:36:28 +0200

    Add restrictions for unregistered users to not access private routes

--- src/containers/RouteGenerator.js
+++ src/containers/RouteGenerator.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import Homepage from '../pages/Homepage';
 import Suits from '../components/Suits';
 import Login from '../pages/Login';
@@ -10,11 +11,11 @@ import Appointments from '../pages/Appointments';
 const RouteGenerator = () => (
   <Switch>
     <Route exact path="/" component={Homepage} />
-    <Route exact path="/suits" component={Suits} />
+    <PrivateRoute exact path="/suits" component={Suits} />
     <Route exact path="/login" component={Login} />
     <Route exact path="/signup" component={SignUp} />
-    <Route exact path="/appointments" component={Appointments} />
-    <Route exact path="/suits/:id" component={SuitItem} />
+    <PrivateRoute exact path="/appointments" component={Appointments} />
+    <PrivateRoute exact path="/suits/:id" component={SuitItem} />
     <Route
       path="/logout"
       render={() => {
@@ -24,5 +25,29 @@ const RouteGenerator = () => (
     />
   </Switch>
 );
+
+/* eslint-disable react/jsx-props-no-spreading */
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  const userToken = localStorage.getItem('userToken');
+  return (
+    <Route
+      {...rest}
+      render={(props) => (userToken === undefined || userToken === 'undefined' || userToken === '' || userToken === null ? (
+        <Redirect
+          to={{
+            pathname: '/login',
+          }}
+        />
+
+      ) : (
+        <Component {...props} />
+      ))}
+    />
+  );
+};
+
+PrivateRoute.propTypes = {
+  component: PropTypes.func.isRequired,
+};
 
 export default RouteGenerator;
