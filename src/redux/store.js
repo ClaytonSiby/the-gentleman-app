2c1906b9e75338d3b6e5137a7844821f0d547349
ClaytonSiby <claytonsiby@gmail.com>
2021-06-14 21:40:04 +0200

    Remove debugging instructions from the browser

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,4 @@
 import thunk from 'redux-thunk';
-import { logger } from 'redux-logger';
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { persistStore } from 'redux-persist';
@@ -7,7 +6,7 @@ import persistReducer from './rootReducer';
 
 const store = createStore(
   persistReducer,
-  composeWithDevTools(applyMiddleware(logger, thunk)),
+  composeWithDevTools(applyMiddleware(thunk)),
 );
 
 const persistor = persistStore(store);
